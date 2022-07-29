6101b8fef9df7c3bb1e14255ade75ffbdde90f9d
eisenhardtleo <100302858+eisenhardtleo@users.noreply.github.com>
2022-07-29 18:49:43 +0200

    Update Card.js
    added button

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,13 @@ class Card extends Component {
     return (
         <div className='col'>
             <div className="card" style={{ width: '18rem', textAlign : "center" }}>
-            <button className="btn btn-primary" onClick={()=>this.props.onIncrement(this.props.card)}>Aggiungi<span className='badge badge-light'>{this.props.quantità}</span></button>
+            <button className="btn btn-success" onClick={()=>this.props.onIncrement(this.props.card)}>+</button>
+            <button className="btn btn-danger" onClick={()=>this.props.onDecrement(this.props.card)}>-</button>
             <img src={this.props.immagine} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{this.props.nome}</h5>
                 <p className="card-text">{this.props.prezzo}€</p>
-                <button className="btn btn-outline-danger" onClick={()=>this.props.onDelete(this.props.id)}>Elimina</button>
+                <button className="btn btn-outline-danger" onClick={()=>this.props.onDelete(this.props.id)}>Elimina<span style={{color:"black"}} className='badge badge-warning'>{this.props.quantità}</span></button>
             </div>
         </div>
         </div>
